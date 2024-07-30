662afa8a6678d2a5a1f1f7aba35c24199cb73050
Wesley Alves <wesley.info07@gmail.com>
2024-07-29 21:04:17 -0300

    Feat: Criando classe DomainException

---
+++ src/NerdStore.Core/DomainObjects/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace NerdStore.Core.DomainObjects
+{
+    public class DomainException : Exception
+    {
+        public DomainException() { }
+        public DomainException(string message) : base(message) { }
+        public DomainException(string message, Exception innerException) : base(message, innerException) { }        
+    }
+}
