f230a25914f009be50ec4c1922ed1d2376157678
Wesley Alves <wesley.info07@gmail.com>
2024-08-03 21:20:21 -0300

    Feat: Configurando Mediatr, AutoMappe Contexto EFCore Catalogo e  aplicando injeções de dependencias na camada de apresentação.

--- src/NerdStore.WebApp.MVC/Program.cs
+++ src/NerdStore.WebApp.MVC/Program.cs
@@ -1,18 +1,32 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using NerdStore.Catalogo.Application.AutoMapper;
+using NerdStore.Catalogo.Data;
 using NerdStore.WebApp.MVC.Data;
+using NerdStore.WebApp.MVC.Setup;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<CatalogoContext>(options =>
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAutoMapper(typeof(DomainToViewModelMappingProfile), typeof(ViewModelToDomainMappingProfile)); //configuração do autommaper: classes que implementam a classe Profile.
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly)); //dependencia do mediatr
+
+builder.Services.AddDependencyInjection();
 
 var app = builder.Build();
 
