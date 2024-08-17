dc257ab6872415c36a7af4407a793ecf3c07bd3f
Wesley Alves <wesley.info07@gmail.com>
2024-08-17 20:45:30 -0300

    Feat - Implementando lógica, para persistência de pedidoItem.

--- src/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
+++ src/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
@@ -1,15 +1,52 @@
-﻿using MediatR;
+﻿using FluentValidation.Internal;
+using MediatR;
 using NerdStore.Core.Messages;
+using NerdStore.Vendas.Domain;
 
 namespace NerdStore.Vendas.Application.Commands
 {
     public class PedidoCommandHandler : IRequestHandler<AdicionarItemPedidoCommand, bool>
     {
+        private readonly IPedidoRepository _pedidoRepository;
+
+        public PedidoCommandHandler(IPedidoRepository pedidoRepository)
+        {
+            _pedidoRepository = pedidoRepository;
+        }
+
         public async Task<bool> Handle(AdicionarItemPedidoCommand message, CancellationToken cancellationToken)
         {
             if (!ValidarComando(message)) return false;
 
-            return true;
+            var pedido = await _pedidoRepository.ObterPedidoRascunhoPorClienteId(message.ClienteId);
+            var pedidoItem = new PedidoItem(message.ProdutoId, message.Nome, message.Quantidade, message.ValorUnitario);
+
+            if (pedido is null)
+            {
+                pedido = Pedido.PedidoFactory.NovoPedidoRascunho(message.ClienteId);
+                pedido.AdicionarItem(pedidoItem);
+
+                _pedidoRepository.Adicionar(pedido);
+            }
+            else
+            {
+                var pedidoItemExistente = pedido.PedidoItemExistente(pedidoItem);
+                pedido.AdicionarItem(pedidoItem);
+
+                if (pedidoItemExistente)
+                {
+                    _pedidoRepository.AtualizarItem(pedido.PedidoItems.FirstOrDefault(item => item.ProdutoId == pedidoItem.ProdutoId));
+                }
+                else
+                {
+                    _pedidoRepository.AdicionarItem(pedidoItem);
+                }
+
+
+            }
+
+
+            return await _pedidoRepository.UnitOfWork.Commit();
         }
 
         private bool ValidarComando(Command message)
