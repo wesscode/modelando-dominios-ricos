96242728e8a5b98f65f0597d61b748600f67c60f
Wesley Alves <wesley.info07@gmail.com>
2024-08-17 21:20:44 -0300

    Fix - Renomenado classe e interface MediatrHandler.

--- src/NerdStore.Core/Bus/IMediatorHandler.cs
+++ src/NerdStore.Core/Bus/IMediatorHandler.cs
@@ -2,8 +2,9 @@
 
 namespace NerdStore.Core.Bus
 {
-    public interface IMediatrHandler
+    public interface IMediatorHandler
     {
         Task PublicarEvento<T>(T evento) where T : Event;
+        Task<bool> EnviarComando<T>(T comando) where T : Command;
     }
 }
