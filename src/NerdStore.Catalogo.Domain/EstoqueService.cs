c1f099580cc1f314d34cb6a19d5520c9b1948d0e
Wesley Alves <wesley.info07@gmail.com>
2024-08-17 21:21:57 -0300

    Feat - Command de adicionar item.

--- src/NerdStore.Catalogo.Domain/EstoqueService.cs
+++ src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -7,10 +7,10 @@ namespace NerdStore.Catalogo.Domain
     public class EstoqueService : IEstoqueService
     {
         private readonly IProdutoRepository _produtoRepository;
-        private readonly IMediatrHandler _bus;
+        private readonly IMediatorHandler _bus;
 
         public EstoqueService(IProdutoRepository produtoRepository,
-                              IMediatrHandler bus)
+                              IMediatorHandler bus)
         {
             _produtoRepository = produtoRepository;
             _bus = bus;
---
+++ src/NerdStore.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Core.Bus;
+using NerdStore.Vendas.Application.Commands;
+
+namespace NerdStore.WebApp.MVC.Controllers
+{
+    public class CarrinhoController : ControllerBase
+    {
+        private readonly IProdutoAppService _produtoAppService;
+        private readonly IMediatorHandler _mediatorHandler;
+
+        public CarrinhoController(IProdutoAppService produtoAppService,
+                                  IMediatorHandler mediatorHandler)
+        {
+            _produtoAppService = produtoAppService;
+            _mediatorHandler = mediatorHandler;
+        }
+
+        [HttpPost]
+        [Route("meu-carrinho")]
+        public async Task<IActionResult> AdicionarItem(Guid id, int quantidade)
+        {
+            var produto = await _produtoAppService.ObterPorId(id);
+            if (produto == null) return BadRequest();
+
+            if (produto.QuantidadeEstoque < quantidade)
+            {
+                TempData["Erro"] = "Produto com estoque insuficiente";
+                return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
+            }
+
+            var command = new AdicionarItemPedidoCommand(ClienteId, produto.Id, produto.Nome, quantidade, produto.Valor);
+            await _mediatorHandler.EnviarComando(command);
+
+            //if (OperacaoValida())
+            //{
+            //    return RedirectToAction("Index");
+            //}
+
+            TempData["Erros"] = "Produto indisponível";
+            return RedirectToAction("ProdutoDetalhe", "Vitrine", new { id });
+        }
+   }
+}
