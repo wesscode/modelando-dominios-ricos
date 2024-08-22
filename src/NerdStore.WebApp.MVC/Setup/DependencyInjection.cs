d97fb595a1634b0c778fcee9a042303ce76b8efd
Wesley Alves <wesley.info07@gmail.com>
2024-08-21 22:08:15 -0300

    Feat - Criando classe onde de captura dos eventos de pedido e resolvendo injeção de dependencia.

--- src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -1,13 +1,14 @@
 ﻿using MediatR;
-using NerdStore.Catalogo.Application.Services;
-using NerdStore.Catalogo.Data.Repository;
+using NerdStore.Vendas.Domain;
 using NerdStore.Catalogo.Domain;
 using NerdStore.Catalogo.Domain.Events;
-using NerdStore.Core.Communication.Mediator;
-using NerdStore.Core.Messages.CommonMessages.Notifications;
-using NerdStore.Vendas.Application.Commands;
 using NerdStore.Vendas.Data.Repository;
-using NerdStore.Vendas.Domain;
+using NerdStore.Catalogo.Data.Repository;
+using NerdStore.Vendas.Application.Events;
+using NerdStore.Core.Communication.Mediator;
+using NerdStore.Vendas.Application.Commands;
+using NerdStore.Catalogo.Application.Services;
+using NerdStore.Core.Messages.CommonMessages.Notifications;
 
 namespace NerdStore.WebApp.MVC.Setup
 {
@@ -30,7 +31,11 @@ namespace NerdStore.WebApp.MVC.Setup
 
             //Vendas
             services.AddScoped<IRequestHandler<AdicionarItemPedidoCommand, bool>, PedidoCommandHandler>();
-            services.AddScoped<IPedidoRepository, PedidoRepository>();                
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
+
+            services.AddScoped<INotificationHandler<PedidoRascunhoIniciadoEvent>, PedidoEventHandler>();
+            services.AddScoped<INotificationHandler<PedidoAtualizadoEvent>, PedidoEventHandler>();
+            services.AddScoped<INotificationHandler<PedidoItemAdicionadoEvent>, PedidoEventHandler>();
         }
     }
 }
