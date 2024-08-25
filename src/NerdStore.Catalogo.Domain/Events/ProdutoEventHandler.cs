fd839cee106b33f580eca5d0a7d4ff03452d3e61
Wesley Alves <wesley.info07@gmail.com>
2024-08-25 20:39:39 -0300

    Feat - Implementação reposição de estoque quando o processamento do pedido for cancelado.

--- src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -6,7 +6,8 @@ namespace NerdStore.Catalogo.Domain.Events
 {
     public class ProdutoEventHandler :
         INotificationHandler<ProdutoAbaixoEstoqueEvent>,
-        INotificationHandler<PedidoIniciadoEvent>
+        INotificationHandler<PedidoIniciadoEvent>,
+        INotificationHandler<PedidoProcessamentoCanceladoEvent>
     {
         private readonly IProdutoRepository _produtoRepository;
         private readonly IEstoqueService _estoqueService;
@@ -40,6 +41,11 @@ namespace NerdStore.Catalogo.Domain.Events
             {
                 await _mediatorHandler.PublicarEvento(new PedidoEstoqueRejeitadoEvent(message.PedidoId, message.ClienteId));
             }
+        }
+
+        public async Task Handle(PedidoProcessamentoCanceladoEvent message, CancellationToken cancellationToken)
+        {
+            await _estoqueService.ReporListaProdutosPedido(message.ProdutosPedido);
         }
     }
 }
