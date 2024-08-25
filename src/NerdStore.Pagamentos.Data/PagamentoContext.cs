ba80aee19338d4e26a5f1a4a011523d88c2ba71a
Wesley Alves <wesley.info07@gmail.com>
2024-08-25 17:46:06 -0300

    Feat  - Adicionando pacote no ef core no p=contexto de pagamento e criando classe de contexto com EFCore.

---
+++ src/NerdStore.Pagamentos.Data/PagamentoContext.cs
@@ -0,0 +1,13 @@
+﻿using NerdStore.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace NerdStore.Pagamentos.Data
+{
+    public class PagamentoContext : DbContext, IUnitOfWork
+    {
+        public Task<bool> Commit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
