ecaffb9aab965e18a125797f635492775de547f8
Wesley Alves <wesley.info07@gmail.com>
2024-08-25 18:04:04 -0300

    Feat - Implementando camada de Data no bounded de context pagamentos.

--- src/NerdStore.Pagamentos.Data/PagamentoContext.cs
+++ src/NerdStore.Pagamentos.Data/PagamentoContext.cs
@@ -1,13 +1,57 @@
 ﻿using NerdStore.Core.Data;
+using NerdStore.Core.Messages;
 using Microsoft.EntityFrameworkCore;
+using NerdStore.Pagamentos.Business;
+using NerdStore.Core.Communication.Mediator;
 
 namespace NerdStore.Pagamentos.Data
 {
     public class PagamentoContext : DbContext, IUnitOfWork
     {
-        public Task<bool> Commit()
+        private readonly IMediatorHandler _mediatorHandler;
+
+        public PagamentoContext(DbContextOptions<PagamentoContext> options, IMediatorHandler mediatorHandler) : base(options)
         {
-            throw new NotImplementedException();
+            _mediatorHandler = mediatorHandler;
+        }
+
+        public DbSet<Pagamento> Pagamentos { get; set; }
+        public DbSet<Transacao> Transacoes { get; set; }
+
+
+        public async Task<bool> Commit()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            var sucesso = await base.SaveChangesAsync() > 0;
+            if (sucesso) await _mediatorHandler.PublicarEventos(this);
+
+            return sucesso;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            modelBuilder.Ignore<Event>();
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PagamentoContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
